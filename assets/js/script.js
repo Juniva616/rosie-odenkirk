6fcf4ada273c7ac6648a0f2d5f6427054cca3d70
Juniva616 <juniva616@gmail.com>
2022-12-07 02:17:21 +0000

    Add interests.html with a google.map and github.html

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+function initmap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 3,
+        center: {
+            lat:  46.619261,
+            lng: -33.134766
+
+        }
+    });
+}
